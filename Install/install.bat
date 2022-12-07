718e0fceddd43d83e7dc5ae8328e2e0cef0c3cb7
BrandonComins <brandcomin@gmail.com>
2022-12-06 23:10:33 -0800

    add folder to help install external libs

---
+++ Install/install.bat
@@ -0,0 +1,13 @@
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+echo =============================
+
+python get-pip.py
+
+echo =============================
+
+pip install -r requirements.txt
+
+echo Done Installing external libraries. Please run Pet.py!
+
+pause
